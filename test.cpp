6612b2c6a591146c53e7837c4d8cc702cde9a711
Jonathan Campbell <jonathan@castus.tv>
2019-01-04 11:40:55 -0800

    more

--- test.cpp
+++ test.cpp
@@ -5,7 +5,41 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+static void help(void) {
+    fprintf(stderr,"-h --help      Help text\n");
+}
+
+static int parse_argv(int argc,char **argv) {
+    char *a;
+    int i=1;
+
+    while (i < argc) {
+        a = argv[i++];
+        if (*a == '-') {
+            do { a++; } while (*a == '-');
+
+            if (!strcmp(a,"h") || !strcmp(a,"help")) {
+                help();
+                return 1;
+            }
+            else {
+                fprintf(stderr,"Unknown switch %s\n",a);
+                return 1;
+            }
+        }
+        else {
+            fprintf(stderr,"Unexpected arg\n");
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc,char **argv) {
+    if (parse_argv(argc,argv))
+        return 1;
+
     return 0;
 }
 
