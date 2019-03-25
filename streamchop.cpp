5479fa663dfc134e36cb215319bf68b5bb312571
Jonathan Campbell <jonathan@castus.tv>
2019-03-24 19:05:57 -0700

    more

---
+++ streamchop.cpp
@@ -0,0 +1,254 @@
+
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
+#include <string>
+
+std::string             opt_prefix;
+std::string             opt_suffix;
+
+unsigned int            replay_time = 0;
+
+time_t                  now = 0;
+time_t                  cut_time = 0;
+time_t                  start_time = 0;
+time_t                  replay_mark_time = 0;
+
+int                     p_fd = -1;
+off_t                   p_fd_replay = -1;
+
+enum {
+    CUT_SEC,
+    CUT_MIN,
+    CUT_HOUR,
+    CUT_DAY
+};
+
+int                     cut_amount = 3;
+int                     cut_unit = CUT_HOUR;
+
+std::string tm2string(struct tm &t) {
+    std::string ret;
+    char tmp[512];
+
+    sprintf(tmp,"%04u-%02u-%02u %02u:%02u:%02u",
+        t.tm_year+1900,
+        t.tm_mon+1,
+        t.tm_mday,
+        t.tm_hour,
+        t.tm_min,
+        t.tm_sec);
+    ret = tmp;
+
+    return ret;
+}
+
+time_t roundsec(struct tm &ct,const int sec) {
+    time_t nt;
+
+    assert(sec <= 60);
+    assert(sec > 0);
+    assert((60 % sec) == 0);/*period must be evenly divisble out of 60*/
+
+    ct.tm_isdst = -1; // need to round according to calendar DST
+    ct.tm_sec -= ct.tm_sec % sec;
+    ct.tm_sec += sec;
+    if (ct.tm_sec >= 60) {
+        ct.tm_sec = 0;
+        ct.tm_min++;
+    }
+    nt = mktime(&ct);
+    if (nt == (time_t)(-1)) abort();
+    return nt;
+}
+
+time_t roundmin(struct tm &ct,const int min) {
+    time_t nt;
+
+    assert(min <= 60);
+    assert(min > 0);
+    assert((60 % min) == 0);/*period must be evenly divisble out of 60*/
+
+    ct.tm_isdst = -1; // need to round according to calendar DST
+    ct.tm_sec  = 0;
+    ct.tm_min -= ct.tm_min % min;
+    ct.tm_min += min;
+    if (ct.tm_min >= 60) {
+        ct.tm_min = 0;
+        ct.tm_hour++;
+    }
+    nt = mktime(&ct);
+    if (nt == (time_t)(-1)) abort();
+    return nt;
+}
+
+time_t roundhour(struct tm &ct,const int hour) {
+    time_t nt;
+
+    assert(hour <= 24);
+    assert(hour > 0);
+    assert((24 % hour) == 0);/*period must be evenly divisble out of 60*/
+
+    ct.tm_isdst = -1; // need to round according to calendar DST
+    ct.tm_sec   = 0;
+    ct.tm_min   = 0;
+    ct.tm_hour -= ct.tm_hour % hour;
+    ct.tm_hour += hour;
+    if (ct.tm_hour >= 24) {
+        ct.tm_hour = 0;
+        ct.tm_mday++;
+    }
+    nt = mktime(&ct);
+    if (nt == (time_t)(-1)) abort();
+    return nt;
+}
+
+time_t roundday(struct tm &ct,const int day) {
+    time_t nt;
+
+    assert(day > 0);
+
+    ct.tm_isdst = -1; // need to round according to calendar DST
+    ct.tm_sec   = 0;
+    ct.tm_min   = 0;
+    ct.tm_hour  = 0;
+
+    ct.tm_mday--; // 1-based
+    ct.tm_mday -= ct.tm_mday % day;
+    ct.tm_mday += day;
+    ct.tm_mday++;
+
+    int p_mon = ct.tm_mon;
+
+    nt = mktime(&ct);/*NTS: mktime() modifies the struct*/
+    if (nt == (time_t)(-1)) abort();
+
+    /* if the interval crosses into the next month, then recompute to cut at the start of the new month */
+    if (p_mon != ct.tm_mon) {
+        ct.tm_mday = 1;
+        return roundday(ct,day);
+    }
+
+    return nt;
+}
+
+static void update_cut_time(const time_t s) {
+    struct tm ct = *localtime(&s);
+    time_t nt;
+
+    printf("Now:      %s\n",tm2string(ct).c_str());
+
+    if (cut_unit == CUT_SEC)
+        nt = roundsec(ct,cut_amount);
+    else if (cut_unit == CUT_MIN)
+        nt = roundmin(ct,cut_amount);
+    else if (cut_unit == CUT_HOUR)
+        nt = roundhour(ct,cut_amount);
+    else if (cut_unit == CUT_DAY)
+        nt = roundday(ct,cut_amount);
+    else
+        abort();
+
+    cut_time = nt;
+    assert(s < cut_time);
+
+    {
+        ct = *localtime(&nt);
+        printf("Cut date: %s\n",tm2string(ct).c_str());
+    }
+}
+
+static void help(void) {
+    fprintf(stderr,"-p prefix\n");
+    fprintf(stderr,"-s suffix\n");
+    fprintf(stderr,"-ca interval amount\n");
+    fprintf(stderr,"-cu interval unit (second, minute, hour, day)\n");
+}
+
+int main(int argc,char **argv) {
+    {
+        char *a;
+        int i=1;
+
+        while (i < argc) {
+            a = argv[i++];
+
+            if (*a == '-') {
+                do { a++; } while (*a == '-');
+
+                if (!strcmp(a,"h")) {
+                    help();
+                    return 1;
+                }
+                else if (!strcmp(a,"ca")) {
+                    a = argv[i++];
+                    cut_amount = atoi(a);
+                }
+                else if (!strcmp(a,"cu")) {
+                    a = argv[i++];
+                    if (!strcasecmp(a,"second"))
+                        cut_unit = CUT_SEC;
+                    else if (!strcasecmp(a,"minute"))
+                        cut_unit = CUT_MIN;
+                    else if (!strcasecmp(a,"hour"))
+                        cut_unit = CUT_HOUR;
+                    else if (!strcasecmp(a,"day"))
+                        cut_unit = CUT_DAY;
+                    else
+                        abort();
+                }
+                else if (!strcmp(a,"p")) {
+                    opt_prefix = argv[i++];
+                }
+                else if (!strcmp(a,"s")) {
+                    opt_suffix = argv[i++];
+                }
+                else {
+                    fprintf(stderr,"Unknown switch %s\n",a);
+                    help();
+                    return 1;
+                }
+            }
+            else {
+                fprintf(stderr,"Unknown arg %s\n",a);
+                return 1;
+            }
+        }
+    }
+
+    /* make sure interval is valid */
+    if (cut_unit == CUT_SEC ||
+        cut_unit == CUT_MIN) {
+        if (cut_amount <= 0 || cut_amount >= 60 || (60 % cut_amount) != 0) {
+            fprintf(stderr,"Invalid interval. Must evenly divide from 60 (try 1, 5, 10, 15, 30...)\n");
+            return 1;
+        }
+    }
+    else if (cut_unit == CUT_HOUR) {
+        if (cut_amount <= 0 || cut_amount >= 24 || (24 % cut_amount) != 0) {
+            fprintf(stderr,"Invalid interval. Must evenly divide from 24 (try 1, 2, 3, 6...)\n");
+            return 1;
+        }
+    }
+    else if (cut_unit == CUT_DAY) {
+        if (cut_amount <= 0 || cut_amount >= 31) {
+            fprintf(stderr,"Invalid interval. Try 1 through 30\n");
+            return 1;
+        }
+    }
+
+    start_time = now = time(NULL);
+    update_cut_time(start_time);
+
+    while (1) {
+
+    }
+
+    return 0;
+}
+
