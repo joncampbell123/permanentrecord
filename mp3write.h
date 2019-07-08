ed2293f27cd7a9d8d45c85c9109e301991d6308c
Jonathan Campbell <jonathan@castus.tv>
2019-07-07 18:13:03 -0700

    more

--- mp3write.h
+++ mp3write.h
@@ -20,23 +20,13 @@ public:
     virtual bool IsOpen(void) const;
     virtual int Write(const void *buffer,unsigned int len);
 private:
-    void _xlat(unsigned char *d,const unsigned char *s,unsigned int len);
-    int _write_xlat(const void *buffer,unsigned int len);
-    int _write_raw(const void *buffer,unsigned int len);
-private:
-    windows_WAVEFORMAT *waveformat(void);
-    windows_WAVEFORMATEX *waveformatex(void);
-    windows_WAVEFORMATEXTENSIBLE *waveformatextensible(void);
-private:
     int             fd;
-    unsigned char   fmt[64];
-    size_t          fmt_size;
     bool            flip_sign;
-    uint32_t        wav_data_start;
-    uint32_t        wav_data_limit;
-    uint32_t        wav_write_pos;
-    unsigned int    bytes_per_sample;
-    unsigned int    block_align;
+    off_t           mp3_write_pos;
+    unsigned int    source_format = 0;
+    uint32_t        source_rate = 0;
+    uint8_t         source_bits_per_sample = 0;
+    uint8_t         source_channels = 0;
     lame_global_flags*  lame_global = NULL;
 private:
     void free_lame(void);
