71e0950e34949e6159d821533bc24085f0332e7c
Jonathan Campbell <jonathan@castus.tv>
2019-08-17 19:52:22 -0700

    more

--- permrec_timerexec.cpp
+++ permrec_timerexec.cpp
@@ -217,8 +217,8 @@ vector<TimeRange>   time_ranges;
 vector<string>      program_args;
 
 bool time_to_run(const time_t _now,const TimeRange &range) {
-    if (range.inverted()) {
-        if (_now < range.begin_time() || _now >= range.end_time())
+    if (range.inverted()) { /* start >= end */
+        if (_now >= range.begin_time() || _now < range.end_time())
             return true;
     }
     else {
