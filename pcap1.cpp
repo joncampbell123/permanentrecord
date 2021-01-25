f13130743ca9cb50670840fb94f3dc03bf89054b
Jonathan Campbell <jonathan@castus.tv>
2021-01-25 13:06:10 -0800

    begin pcap1

---
+++ pcap1.cpp
@@ -0,0 +1,67 @@
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+#pragma pack(push,1)
+typedef struct pcap_hdr_t {
+    uint32_t    magic_number;   /* magic number */
+    uint16_t    version_major;  /* major version number */
+    uint16_t    version_minor;  /* minor version number */
+    int32_t     thiszone;       /* GMT to local correction */
+    uint32_t    sigfigs;        /* accuracy of timestamps */
+    uint32_t    snaplen;        /* max length of captured packets, in octets */
+    uint32_t    network;        /* data link type */
+} pcap_hdr_t;
+#pragma pack(pop)
+
+#pragma pack(push,1)
+typedef struct pcaprec_hdr_t {
+    uint32_t    ts_sec;         /* timestamp seconds */
+    uint32_t    ts_usec;        /* timestamp microseconds */
+    uint32_t    incl_len;       /* number of octets of packet saved in file */
+    uint32_t    orig_len;       /* actual length of packet */
+} pcaprec_hdr_t;
+#pragma pack(pop)
+
+static unsigned char tmpbuf[65536];
+static pcap_hdr_t pcaphdr;
+
+int main(int argc,char **argv) {
+    int fd;
+
+    if (argc < 2) {
+        fprintf(stderr,"%s <pcap file>\n",argv[0]);
+        return 1;
+    }
+
+    fd = open(argv[1],O_RDONLY);
+    if (fd < 0) return 1;
+
+    if (read(fd,tmpbuf,sizeof(pcap_hdr_t)) != sizeof(pcap_hdr_t)) return 1;
+    pcaphdr = *((pcap_hdr_t*)tmpbuf);
+    if (pcaphdr.magic_number != 0xA1B2C3D4) return 1;
+    if (pcaphdr.version_major != 2) return 1;
+    if (pcaphdr.network != 1) return 1;/*must be ethernet*/
+
+    while (read(fd,tmpbuf,sizeof(pcaprec_hdr_t)) == sizeof(pcaprec_hdr_t)) {
+        pcaprec_hdr_t prec = *((pcaprec_hdr_t*)tmpbuf);
+
+        if (prec.orig_len < prec.incl_len) return 1;
+        if (prec.incl_len > pcaphdr.snaplen) return 1;
+        if (prec.incl_len > sizeof(tmpbuf)) return 1;
+        if (prec.incl_len == 0) continue;
+
+        if (read(fd,tmpbuf,prec.incl_len) != prec.incl_len) return 1;
+
+        // TODO
+    }
+
+    close(fd);
+    return 0;
+}
+
